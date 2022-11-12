54faea18afdf67a9bd224cf2289b13253f4be4a1
timmashkov <106866033+timmashkov@users.noreply.github.com>
2022-11-12 14:37:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,2 @@
-import logging
-
-import telebot
-from telebot import types
-
-logger = telebot.logger
-telebot.logger.setLevel(logging.DEBUG)  # Outputs debug messages to console.
-
-bot = telebot.TeleBot('5277345966:AAGxh5pqqilQsyjAOn7ADNC893m6Er9F12c')
-
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "PIDORAS")
-
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-
-
-bot.infinity_polling()
+text = [i for i in input().split() if len(i) <= 3]
+print(text)
