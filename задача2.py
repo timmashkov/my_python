d7c3f529a39246f5cc169995d7cce6c5e2cf550c
timmashkov <106866033+timmashkov@users.noreply.github.com>
2022-11-28 11:39:43 +0300

    Update задача2.py

--- задача2.py
+++ задача2.py
@@ -1,31 +1,30 @@
-def inputKoord(x):
-    a = [0] * x
-    for i in range(x):
-        is_OK = False
-        while not is_OK:
-            try:
-                number = float(input(f"Введите {i + 1} координату: "))
-                a[i] = number
-                is_OK = True
-                if a[i] == 0:
-                    is_OK = False
-                    print("Координата не должно быть равна 0 ")
-            except ValueError:
-                print("Ты ошибся. Вводить надо числа!")
-    return a
+""" Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+Пример:
+- пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4) """
 
 
-def checkCoordinates(xy):
-    text = 4
-    if xy[0] > 0 and xy[1] > 0:
-        text = 1
-    elif xy[0] < 0 and xy[1] > 0:
-        text = 2
-    elif xy[0] < 0 and xy[1] < 0:
-        text = 3
-    print(f"Точка находится в {text} четверти плоскости")
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Число должно быть integer ")
+    return number
 
 
-koordinate = inputKoord(2)
-checkCoordinates(koordinate)
+def mult(n):
+    if n == 1:
+        return 1
+    else:
+        return n * mult(n - 1)
 
+
+num = InputNumbers("Введите число: ")
+
+list = []
+for e in range(1, num + 1):
+    list.append(mult(e))
+
+print(f"Произведения чисел от 1 до {num}:  {list}")
