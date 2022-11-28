fe06b42370c5c88098b037621e092694939a4506
timmashkov <106866033+timmashkov@users.noreply.github.com>
2022-11-28 11:40:00 +0300

    Update задача3.py

--- задача3.py
+++ задача3.py
@@ -1,26 +1,3 @@
-def inputNumbers(x):
-    xy = ["X", "Y"]
-    a = []
-    for i in range(x):
-        is_OK = False
-        while not is_OK:
-            try:
-                number = int(input(f"Введите координату по {xy[i]}: "))
-                a.append(number)
-                is_OK = True
-            except ValueError:
-                print("Ты ошибся. Вводить надо целые числа!")
-    return a
-
-
-def calculateLengthSegment(a, b):
-    lengthSegment = ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5)
-    return lengthSegment
-
-
-print("Введите координаты точки А")
-pointA = inputNumbers(2)
-print("Введите координаты точки В")
-pointB = inputNumbers(2)
-
-print(f"Длина отрезка: {format(calculateLengthSegment(pointA, pointB), '.2f')}")
+n = int(input())
+lst = [round((1+1/i)**i, 3) for i in range(1, n+1)]
+print(f'Последовательность: {lst}\nСумма: {round(sum(lst), 3)}')
