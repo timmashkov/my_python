19575658fd6dfd5edacf0d8cd32af01d27486d2d
timmashkov <106866033+timmashkov@users.noreply.github.com>
2022-11-29 11:59:21 +0300

    Update задача1.py

--- задача1.py
+++ задача1.py
@@ -1,28 +1,6 @@
-""" Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
-Пример:
-- 6782 -> 23
-- 0,56 -> 11 """
-
-
-def InputNumbers(inputText):
-    is_OK = False
-    while not is_OK:
-        try:
-            number = float(input(f"{inputText}"))
-            is_OK = True
-        except ValueError:
-            print("Это не число!")
-    return number
-
-
-def sumNums(num):
-    sum = 0
-    for i in str(num):
-        if i != ".":
-            sum += int(i)
-    return sum
-
-
-num = InputNumbers("Введите число: ")
-
-print(f"Сумма цифр = {sumNums(num)}")
+numbers = [int(i) for i in input().split()]
+count = 0
+for num in range(len(numbers)):
+    if num % 2 != 0:
+        count += numbers[num]
+print(count)
