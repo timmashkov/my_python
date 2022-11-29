b18fd03b2f0c049cee4acfa7d5e88dc7a9981b13
timmashkov <106866033+timmashkov@users.noreply.github.com>
2022-11-29 12:06:09 +0300

    Update задача3.py

--- задача3.py
+++ задача3.py
@@ -1,3 +1,22 @@
-n = int(input())
-lst = [round((1+1/i)**i, 3) for i in range(1, n+1)]
-print(f'Последовательность: {lst}\nСумма: {round(sum(lst), 3)}')
+from random import uniform
+
+
+n = int(input('Введите размер списка '))
+list1 = []
+for i in range(n):
+    f = uniform(0, 9)
+    list1.append(round(f, 2))
+
+min = list1[0]
+max = 0
+for i in range(len(list1)):
+    
+    if max < list1[i]:
+        max = list1[i]
+    if min > list1[i]:
+        min = list1[i]
+dif = (max - int(max)) - (min - int(min))
+
+print(list1)
+print(max, min)
+print(round(dif,2))
