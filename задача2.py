d8ccd3b064e61c75063f04bd0d160539ed1388a8
timmashkov <106866033+timmashkov@users.noreply.github.com>
2022-12-15 09:40:51 +0300

    Update задача2.py

--- задача2.py
+++ задача2.py
@@ -1,15 +1,12 @@
 
-#Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+#Дан список, вывести отдельно буквы и цифры.
 
-num = int(input("Введите число: "))
-i = 2 # первое простое число
-lst = []
-old = num
-while i <= num:
-    if num % i == 0:
-        lst.append(i)
-        num //= i
-        i = 2
-    else:
-        i += 1
-print(f"Простые множители числа {old} приведены в списке: {lst}")
+import methods as m
+
+length = m.enter_num('Введите размер массива: ')
+print('Сгенерированный массив: ')
+lst = m.create_list(length)
+print(lst)
+
+print('Отсортированный массив: ')
+print(m.separate_by_elements(lst))
