f2c5c5c0b86ceb0f12910954aea2f7ad859888db
timmashkov <106866033+timmashkov@users.noreply.github.com>
2022-12-15 09:42:46 +0300

    Update задача3.py

--- задача3.py
+++ задача3.py
@@ -1,8 +1,9 @@
-#Задайте последовательность чисел. 
-# Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+#Преобразовать набора списков users = ['user1','user2','user3','user4','user5'] ids = [4, 5, 9, 14, 7] salary = [111,222,333] в другой набор
 
-lst = list(map(int, input("Введите числа через пробел:\n").split()))
-print(f"Исходный список: {lst}")
-new_lst = []
-[new_lst.append(i) for i in lst if i not in new_lst]
-print(f"Список из неповторяющихся элементов: {new_lst}")
+from numpy import array
+from pandas.core.common import flatten
+
+l = [[1, 2, 3], [4, 5, 6]]
+
+print(list(array(l).flat))
+print(list(flatten(l)))
