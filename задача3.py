498863b3e63d205d6ed2ad6ab47f751ff81fcac0
timmashkov <106866033+timmashkov@users.noreply.github.com>
2022-12-18 06:22:18 +0300

    Update and rename задача3.py to part4.py

--- задача3.py
+++
@@ -1,9 +0,0 @@
-#Преобразовать набора списков users = ['user1','user2','user3','user4','user5'] ids = [4, 5, 9, 14, 7] salary = [111,222,333] в другой набор
-
-from numpy import array
-from pandas.core.common import flatten
-
-l = [[1, 2, 3], [4, 5, 6]]
-
-print(list(array(l).flat))
-print(list(flatten(l)))
