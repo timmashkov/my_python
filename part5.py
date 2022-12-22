7896ba438c3199223d014f293c35c945ef3c9e7c
timmashkov <106866033+timmashkov@users.noreply.github.com>
2022-12-22 10:11:43 +0300

    Update part5.py

--- part5.py
+++ part5.py
@@ -1,7 +1,7 @@
 
 def print_data(data):
     if len(data) > 0:
-        print("Фамилия".center(20), "Имя".center(20), "Отчество".center(20), "Дата рождения".center(20), "Телефон".center(15), "Категория".center(30))
+        print("Фамилия".center(20), "Имя".center(20), "Отчество".center(20), "Дата рождения".center(20), "Группа".center(15), "Поток".center(30))
         print("-"*130)
         for item in data:
             print(item[0].center(20), item[1].center(20), item[2].center(20), item[3].center(20), item[4].center(15), item[5].center(30))
@@ -9,7 +9,7 @@ def print_data(data):
         print("Справочник пуст!")
         
 
-# модуль поиска контакта
+# модуль поиска группы
 
 from export_data import export_data
 from print_data import print_data
